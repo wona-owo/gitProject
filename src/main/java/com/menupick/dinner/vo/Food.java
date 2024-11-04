9dd0062b080d16ab13156dc09df234888b6a7fa3
태욱 <gkax0dnr222@gmail.com>
2024-11-04 18:03:48 +0900

    dinner dao,service,vo객체 생성

---
+++ src/main/java/com/menupick/dinner/vo/Food.java
@@ -0,0 +1,16 @@
+package com.menupick.dinner.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Food {
+	private String foodNo;
+	private String foodName;
+	private String foodNation;
+	private String foodCate;
+	
+}
