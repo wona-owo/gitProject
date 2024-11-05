0cfbf0ef866a874c1f4827e07a38973b4d5c8ade
DESKTOP-B7UOA3S\user1 <cyan0351@gmail.com>
2024-11-05 18:56:46 +0900

    로그인 백 구현(11/05)

--- src/main/java/com/menupick/member/model/dao/MemberDao.java
+++ src/main/java/com/menupick/member/model/dao/MemberDao.java
@@ -1,14 +1,54 @@
 package com.menupick.member.model.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import com.menupick.common.JDBCTemplate;
 import com.menupick.member.model.vo.Member;
 
 public class MemberDao {
 
 	public Member memberLogin(Connection conn, String loginId, String loginPw) {
-
-		return null;
+		
+		PreparedStatement pstmt = null;
+		ResultSet rset = null;
+		Member m = null;
+		
+		String query ="select * from tbl_member where member_id =? and member_pw =?";
+		
+		try {
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, loginId);
+			pstmt.setString(2, loginPw);
+			rset = pstmt.executeQuery();
+			
+			if(rset.next()) {
+				m = new Member();
+				m.setMemberNo(rset.getString("member_no"));
+				m.setMemberId(rset.getString("member_id"));
+				m.setMemberPw(rset.getString("member_pw"));
+				m.setMemberName(rset.getString("member_name"));
+				m.setMemberNick(rset.getString("member_nick"));
+				m.setMemberPhone(rset.getString("member_phone"));
+				m.setMemberAddr(rset.getString("member_addr"));
+				m.setMemberGender(rset.getString("member_gender"));
+				m.setMemberEmail(rset.getString("member_email"));
+				m.setEnrollDate(rset.getString("enroll_date"));
+				m.setAdultValid(rset.getString("adult_valid"));
+				m.setMemberLevel(rset.getInt("member_level"));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			JDBCTemplate.close(rset);
+			JDBCTemplate.close(pstmt);
+		}
+	
+		return m;
 	}
 
 }
