cb60107e0fbc3f73dc285e87a59db7ea681b5422
명현 <rlaaudgus4086@gmail.com>
2024-11-11 16:09:30 +0900

    MemberLoginFrmServlet 의 url member/loginFrm 으로 변경

--- src/main/java/com/menupick/member/controller/MemberLoginFrmServlet.java
+++ src/main/java/com/menupick/member/controller/MemberLoginFrmServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class MemberLoginServlet
  */
-@WebServlet("/member/login")
+@WebServlet("/member/loginFrm")
 public class MemberLoginFrmServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
