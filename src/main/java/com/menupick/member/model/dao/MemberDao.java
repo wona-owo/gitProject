0a2122bbb380563affb6999ae49c0746750b0942
명현 <rlaaudgus4086@gmail.com>
2024-11-12 12:04:20 +0900

    회원가입기능 구현완료

--- src/main/java/com/menupick/member/model/dao/MemberDao.java
+++ src/main/java/com/menupick/member/model/dao/MemberDao.java
@@ -114,9 +114,9 @@ public class MemberDao {
 		PreparedStatement pstmt = null;
 		int result = 0;
 		
-		String query = "insert into tbl_member (member_no, member_id, member_pw, member_name, member_nick, member_phone, member_addr, member_gender, member_email, enroll_date, adult_confirm, member_level) " +
-	               "values (seq_member.nextval, ?, ?, ?, ?, ?, ?, ?, ?, sysdate, 'N', 2)";
-		/*String query = "insert into tbl_member values(member_seq.nextval, ?, ?, ?, ?, ?, ?, ?, ?, sysdate, N, 2)";*/
+		String query ="insert into tbl_member (member_no, member_id, member_pw, member_name, member_nick, member_phone, member_addr, member_gender, member_email, enroll_date, adult_confirm, member_level) " +
+	               "values (seq_member.nextval, ?, ?, ?, ?, ?, ?, ?, ?, sysdate, 'n', 2)";
+		//String query = "insert into tbl_member values (to_char(seq_member.nextval), ?, ?, ?, ?, ?, ?, ?, ?, sysdate, 'N', 2)";
 
 		try {
 			pstmt = conn.prepareStatement(query);
