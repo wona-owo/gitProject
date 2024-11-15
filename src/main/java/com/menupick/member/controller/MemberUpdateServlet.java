936b6df8c7ead6b2f215a79906ff74b65db1d0da
명현 <rlaaudgus4086@gmail.com>
2024-11-15 09:11:06 +0900

    회원정보 수정 서블릿 수정

--- src/main/java/com/menupick/member/controller/MemberUpdateServlet.java
+++ src/main/java/com/menupick/member/controller/MemberUpdateServlet.java
@@ -31,55 +31,7 @@ public class MemberUpdateServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// 회원 정보 수정 페이지로 이동
 	    request.getRequestDispatcher("/WEB-INF/views/member/update.jsp").forward(request, response);
-	/*			String memberNo = request.getParameter("memberNo");
-				String memberId = request.getParameter("memberId");
-				String memberPw = request.getParameter("memberPw");
-				String memberName = request.getParameter("memberName");
-				String memberNick = request.getParameter("memberNick");
-				String memberPhone = request.getParameter("memberPhone");
-				String memberAddr = request.getParameter("memberAddr");
-				String memberEmail = request.getParameter("memberEmail");
-
-				Member updMember = new Member();
-				updMember.setMemberNo(memberNo);
-				updMember.setMemberId(memberId);
-				updMember.setMemberPw(memberPw);
-				updMember.setMemberName(memberName);
-				updMember.setMemberNick(memberNick);
-				updMember.setMemberPhone(memberPhone);
-				updMember.setMemberAddr(memberAddr);
-				updMember.setMemberEmail(memberEmail);
-				
-				
-				MemberService service = new MemberService();
-				int result = service.updateMember(updMember);
-				
-				// 결과 처리
-		        HttpSession session = request.getSession(false);
-		        if (result > 0 && session != null) {
-		            // 세션에 저장된 회원 정보도 업데이트
-		            Member sessionMember = (Member) session.getAttribute("loginMember");
-		            sessionMember.setMemberId(memberId);
-		            sessionMember.setMemberPw(memberPw);
-		            sessionMember.setMemberName(memberName);
-		            sessionMember.setMemberNick(memberNick);
-		            sessionMember.setMemberPhone(memberPhone);
-		            sessionMember.setMemberAddr(memberAddr);
-		            sessionMember.setMemberEmail(memberEmail);
-		            
-		         // 성공 메시지 설정
-		            request.setAttribute("title", "알림");
-		            request.setAttribute("msg", "회원 정보가 수정되었습니다. 재로그인 하시기 바랍니다.");
-		            request.setAttribute("icon", "success");
-		            request.setAttribute("loc", "/member/login");
-		        } else {
-		            // 오류 발생 시 메시지 설정
-		            request.setAttribute("title", "알림");
-		            request.setAttribute("msg", "회원 정보 수정 중 오류가 발생했습니다.");
-		            request.setAttribute("icon", "error");
-		            request.setAttribute("loc", "/member/mypage");
-		        }
-				request.getRequestDispatcher("/WEB-INF/views/member/update.jsp").forward(request, response);*/
+	
 			}
 
 	/**
@@ -126,6 +78,9 @@ public class MemberUpdateServlet extends HttpServlet {
                 sessionMember.setMemberNick(memberNick);
                 sessionMember.setMemberPhone(memberPhone);
                 sessionMember.setMemberAddr(memberAddr);
+                sessionMember.setMemberAddr(zipp_code);
+                sessionMember.setMemberAddr(userAdd1);
+                sessionMember.setMemberAddr(userAdd2);
                 sessionMember.setMemberEmail(memberEmail);
             }
 
